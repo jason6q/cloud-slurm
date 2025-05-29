23cc2399bb55bf20147838df358be75104e22d09
nu1ptr <j6quach@gmail.com>
2025-05-29 00:02:08 -0500

    SLURM envs

--- stool.py
+++ stool.py
@@ -12,7 +12,7 @@ from omegaconf import OmegaConf
 class StoolArgs:
     config: str = ""
     file: str = ""
-    mlflow_uri: str = "localhost:5000"
+    mlflow_uri: str = "http://localhost:5000"
     job_name: str = "toy"
     nodes: int = 1
     ngpu: int = 1
@@ -37,7 +37,19 @@ SBATCH_CMD = """#!/bin/bash
 #SBATCH --partition={partition}
 #SBATCH --output={output_log}
 
-export MLFLOW_TRACK_URI={mlflow_uri}
+export MASTER_PORT=12355
+export WORLD_SIZE=$(($SLURM_NNODES * $SLURM_NTASKS_PER_NODE))
+
+master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
+export MASTER_ADDR=$master_addr
+
+echo "WORLD_SIZE="$WORLD_SIZE
+echo "MASTER_PORT="$MASTER_PORT
+echo "MASTER_ADDR="$MASTER_ADDR
+
+export MLFLOW_TRACKING_URI={mlflow_uri}
+export MLFLOW_UI_DEFAULT_CHART_TYPE=line
+
 conda activate {conda_env}
 """
 
