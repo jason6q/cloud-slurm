8e30f535f9f9d61cc2d6e1174016b9ae01c9d09b
nu1ptr <j6quach@gmail.com>
2025-06-02 22:50:21 -0500

    init

---
+++ train-full/aimv2.py
@@ -0,0 +1,11 @@
+"""
+    REFERENCES:
+        Paper for AimV2: https://arxiv.org/pdf/2411.14402
+        Github for AimV2: https://github.com/apple/ml-aim
+"""
+
+import torch
+
+class AIMv2:
+    def __init__():
+        return
